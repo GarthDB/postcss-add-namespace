250ba11a6d08b900858be567eb0278e1bba51a01
garthdb <garthdb@gmail.com>
2016-08-18 01:20:41 -0600

    added documentation

--- src/add-namespace.js
+++ src/add-namespace.js
@@ -1,6 +1,18 @@
 const debug = require('debug')('postcss-add-namespace');
 
-// Returns true if a selector matches a test
+/**
+ *  Public:
+ *
+ *  * `selector` {String} - css rule selector to check.
+ *  * `test` {String} or {RegExp} to check `selector`
+ *
+ *  ## Example
+ *
+ *    selectorMatchesTest('.icon', '.icon');
+ *    // returns true
+ *
+ *  Returns {Bool}
+ */
 function selectorMatchesTest(selector, test) {
   if (test instanceof RegExp) {
     return test.exec(selector);
@@ -10,7 +22,20 @@ function selectorMatchesTest(selector, test) {
   return false;
 }
 
-// Returns true if a selector matches any test
+/**
+ *  Public:
+ *
+ *  * `selector` {String} selector to check if it matches the tests.
+ *  * `test` {String} or {Array} of {Strings} to use to check.
+ *  * `fallback` {Bool} if no `test` are added, it will return `fallback`.
+ *
+ *  ## Example
+ *
+ *    selectorMatchesTests('.icon', '.icon', true);
+ *    // returns true
+ *
+ *  Returns {Bool}
+ */
 function selectorMatchesTests(selector, test, fallback) {
   // Convert strings to arrays
   const tests = (typeof test === 'string') ? [test] : test;
@@ -21,8 +46,35 @@ function selectorMatchesTests(selector, test, fallback) {
     selectorMatchesTest(selector, newTest)
   );
 }
-
-export default class Inherit {
+/**
+ *  AddNamespace Class
+ */
+export default class AddNamespace {
+  /**
+   *  Public: initiates AddNamespace object and loads options
+   *
+   *  * `css` parsed postcss css.
+   *  * `namespace` (optional) the {String} added to the beginning of classes.
+   *  * `opts` (optional) {Object} of options that determine which classes will
+   *    have the namespace prefix added. Expected properties:
+   *    * `not` specific classes or classes that match a regex to skip.
+   *    * `only` specific classes or classes that match a regex to prefix.
+   *
+   *  ## Examples
+   *
+   *  Designed for use in a postcss plugin.
+   *
+   *    var postcss = require('postcss');
+   *    var addNamespace = require('./add-namespace');
+   *    exports.default = postcss.default.plugin('postcss-add-namespace', function (namespace) {
+   *      var opts = arguments.length <= 1 || arguments[1] === undefined ? {} : arguments[1];
+   *      return function (css) {
+   *        return new addNamespace(css, namespace, opts);
+   *      };
+   *    });
+   *
+   *  Returns postcss {Object} with result property (`css`).
+   */
   constructor(css, namespace = '', opts = {}) {
     this.root = css;
     this.ns = namespace;
@@ -38,9 +90,39 @@ export default class Inherit {
       debug(`${selector} replaced with ${rule.selector}`);
     });
   }
+  /**
+   *  Private: checks if selector should be prefixed with the namespace.
+   *
+   *  * `selector` {String} of rule selector.
+   *
+   *  ## Example
+   *
+   *  With `{ only: '.icon' }`
+   *
+   *    shouldIncludeSelector('.icon { prop: value; }');
+   *
+   *  Will return `true`
+   *
+   *  Returns {Bool}
+   */
   shouldIncludeSelector(selector) {
     return selectorMatchesTests(selector, this.only, true);
   }
+  /**
+   *  Private: checks if selector should be ignored while adding the namespace prefix.
+   *
+   *  * `selector` {String} of rule selector.
+   *
+   *  ## Example
+   *
+   *  With `{ not: '.icon' }`
+   *
+   *    shouldIgnoreSelector('.icon { prop: value; }');
+   *
+   *  Will return `false`
+   *
+   *  Returns {Bool}
+   */
   shouldIgnoreSelector(selector) {
     return selectorMatchesTests(selector, this.not, false);
   }
